fe0a04013f864f3fdb76312ea958d9133a9b6429
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-26 23:04:42 -0300

    Formatando área QuizContainer

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,23 @@ const BackgroundImage = styled.div`
   
 `;
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
   return (
     <BackgroundImage>
-      Olá mundo
+      <QuizContainer>
+        Olá mundo
+      </QuizContainer>
     </BackgroundImage>
   );
 }
