2ab6a4a64bd55fc2b00145ab5d84a8add4d9ec93
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-26 23:11:37 -0300

    Criando e formatando componente Widget

--- pages/index.js
+++ pages/index.js
@@ -20,11 +20,79 @@ export const QuizContainer = styled.div`
   }
 `;
 
+export const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4CAF50;
+  background-color: #1C1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+
 export default function Home() {
   return (
     <BackgroundImage>
       <QuizContainer>
-        Olá mundo
+
+        <Widget>
+
+        <Widget.Header>
+            <h1>The legend of zelda</h1>
+        </Widget.Header>
+
+          <Widget.Content>
+           <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>  
+                  
+        </Widget> 
+
+        <Widget>
+          <Widget.Content>
+           <h1>The legend of zelda</h1>
+           <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>          
+        </Widget>
+
+
       </QuizContainer>
     </BackgroundImage>
   );
