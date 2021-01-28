ae1b631ab71309ce2f5363344f49c91faecc04f4
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-28 01:22:48 -0300

    Exibindo quantidade de acertos com filter

--- pages/quiz.js
+++ pages/quiz.js
@@ -32,14 +32,7 @@ function ResultWidget({ results }) {
       </Widget.Header>
 
       <Widget.Content>
-        <p>Você acertou {' '} {results.reduce((somatoriaAtual, resultAtual) => {
-          const isAcerto = resultAtual === true;
-          if (isAcerto) {
-            return somatoriaAtual + 1;
-          }
-          return somatoriaAtual;
-        }, 0)} perguntas
-        </p>
+        <p>Você acertou {' '} {results.filter((x) => x).length} perguntas</p>
         <ul>
           {results.map((result) => (
             <li>#01 Resultado:{result === true ? 'Acertou' : 'Errou'}</li>
